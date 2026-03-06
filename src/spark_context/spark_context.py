30b455a11af29c60f6fcb0fc8e148f8ebd58ed33
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-06 19:37:40 +0530

    created spark context here

---
+++ src/spark_context/spark_context.py
@@ -0,0 +1,15 @@
+from pyspark import SparkConf, SparkContext
+
+# Create Spark configuration
+conf = SparkConf() \
+    .setAppName("MySparkApplication") \
+    .setMaster("local[*]") \
+    .set("spark.executor.memory", "2g") \
+    .set("spark.driver.memory", "1g")
+
+# Create SparkContext
+sc = SparkContext(conf=conf)
+
+# Verify SparkContext
+print("SparkContext Created")
+print("Spark Version:", sc.version)
