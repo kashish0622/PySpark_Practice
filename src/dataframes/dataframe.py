e2c3ee92115a4daa096f62a98411ff7b26f8a8ae
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-09 15:31:27 +0530

    created a dataframe and added a sample data

--- src/dataframes/dataframe.py
+++ src/dataframes/dataframe.py
@@ -1,6 +1,21 @@
-# creating data frame here
+# creating session here
+from pyspark.sql import SparkSession
 spark = SparkSession.builder\
     .appName("PySpark")\
     .master("local[*]")\
     .getOrCreate()
 print("Spark Session Created Successfully")
+
+# creation dataframe
+data = [
+    (1, "Kashish", 21),
+    (2, "Rahul", 23),
+    (3, "Ananya", 20),
+    (4, "Aman", 22)
+]
+
+columns = ["id", "name", "age"]
+
+df = spark.createDataFrame(data, columns)
+
+df.show()
