b4e2e1607558cf637b912660eb7a2c57c3949888
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-10 15:15:19 +0530

    configured the spark session

--- src/generaldffunctions/general.py
+++ src/generaldffunctions/general.py
@@ -1,4 +1,5 @@
 #created spark session
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName("General_Functions").master("local[*]").getOrCreate()
+spark = SparkSession.builder.appName("General_Functions").master("local[*]").config("spark.executor.memory", "5g").config("spark.executor.cores", "3").config("spark.driver.memory", "2g").getOrCreate()
 print("Spark Session for General_Functions Created Successfully")
+
