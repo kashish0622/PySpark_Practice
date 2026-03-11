5ea7d7bc033b54e7736c39b3a8ff346aeae7ce1f
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-11 17:04:27 +0530

    row number column added using row_number() function

--- src/windowfunctions/window.py
+++ src/windowfunctions/window.py
@@ -10,4 +10,21 @@ spark = SparkSession.builder.appName("WindowFunctions")\
     .getOrCreate()
 print("SparkSession created for window functions")
 
+data = [
+    (101, "Ayush", "IT", 50000),
+    (102, "Riya", "HR", 45000),
+    (103, "Karan", "IT", 60000),
+    (104, "Sneha", "HR", 48000),
+    (105, "Rahul", "IT", 52000)
+]
+
+columns = ["emp_id", "name", "department", "salary"]
+
+df = spark.createDataFrame(data, columns)
+df.show()
+
 window_spec = Window.partitionBy("department").orderBy(desc("salary"))
+df.withColumn(
+    "row_number",
+    row_number().over(window_spec)
+).show()
