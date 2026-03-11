ca64cac3fa67dde6c415bc3f021f6f68c7463ff9
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-11 13:24:50 +0530

    dataset added for practice

--- src/mathematicalfunctions/math.py
+++ src/mathematicalfunctions/math.py
@@ -8,3 +8,16 @@ spark = SparkSession.builder.appName("Mathematical_functions")\
     .getOrCreate()
 print("SparkSession created for Mathematical_functions")
 
+data = [
+    (101, "Ayush", -25.5, 4),
+    (102, "Riya", -10.2, 9),
+    (103, "Karan", 15.7, 16),
+    (104, "Sneha", 8.3, 25),
+    (105, "Rahul", -3.6, 36)
+]
+
+columns = ["id", "name", "value", "number"]
+
+df = spark.createDataFrame(data, columns)
+df.cache()
+
