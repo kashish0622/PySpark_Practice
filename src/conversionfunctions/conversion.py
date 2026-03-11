e00f234f0c7d8530cd7bda263615485e747c0f5d
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-11 14:41:02 +0530

    spark session created

---
+++ src/conversionfunctions/conversion.py
@@ -0,0 +1,22 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+spark = SparkSession.builder.appName("ConversionFunctions")\
+    .master("local[*]")\
+    .config("spark.driver.memory","3g")\
+    .config("spark.executor.memory","2g")\
+    .config("spark.executors.cores", "2")\
+    .getOrCreate()
+print("SparkSession created for conversion functions")
+
+data = [
+    (101, "Ayush", "25", "50000.50"),
+    (102, "Riya", "30", "45000.75"),
+    (103, "Karan", "28", "60000.20"),
+    (104, "Sneha", "26", "55000.10"),
+    (105, "Rahul", "32", "52000.60")
+]
+
+columns = ["emp_id", "name", "age_string", "salary_string"]
+
+df = spark.createDataFrame(data, columns)
+df.cache()
