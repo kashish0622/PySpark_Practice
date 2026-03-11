8a6e6cd6bfceffa33496579a0c3e916bada02b56
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-11 12:48:38 +0530

    cache in df practised

---
+++ src/cachenpersist/cache.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder\
+    .appName("PySpark")\
+    .master("local[*]")\
+    .getOrCreate()
+print("Spark Session Created Successfully")
+
+# creation dataframe
+data = [
+    (1, "Kashish", 21),
+    (2, "Rahul", 23),
+    (3, "Ananya", 20),
+    (4, "Aman", 22)
+]
+
+columns = ["id", "name", "age"]
+
+df = spark.createDataFrame(data, columns)
+df.cache()
+df.show()
+df.filter(df.age > 21).show()
+df.unpersist()
+
+spark.stop()
