d80ef9a6b8fe8ee15386d250735d127d43dc82cb
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-11 16:14:37 +0530

    array_contain() function is applied here

--- src/arrayfunctions/array.py
+++ src/arrayfunctions/array.py
@@ -1,12 +1,12 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-spark = SparkSession.builder.appName("Mathematical_functions")\
+spark = SparkSession.builder.appName("Array_functions")\
     .master("local[*]")\
     .config("spark.driver.memory", "5g")\
     .config("spark.executor.memory", "3g")\
     .config("spark.executor.cores", "3")\
     .getOrCreate()
-print("SparkSession created for Mathematical_functions")
+print("SparkSession created for Array_functions")
 
 data = [
     (101, "Ayush", "Python", "SQL", "Spark"),
@@ -20,4 +20,11 @@ columns = ["emp_id", "name", "skill_set1", "skill_set2", "skill_set3"]
 
 df = spark.createDataFrame(data, columns)
 df.cache()
-df.select(array(col("skill_set1"),col("skill_set2")).alias("skills")).show()
+#df.select(array(col("skill_set1"),col("skill_set2")).alias("skills")).show()
+df.select(
+    "name",
+    array_contains(
+        array(col("skill_set1"), col("skill_set2"), col("skill_set3")),
+        "Python"
+    ).alias("knows_python")
+).show()
