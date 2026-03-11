73ed843aaa872e3011a712dd42419c992a1988f0
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-11 15:40:41 +0530

    array() function applied

--- src/arrayfunctions/array.py
+++ src/arrayfunctions/array.py
@@ -9,14 +9,15 @@ spark = SparkSession.builder.appName("Mathematical_functions")\
 print("SparkSession created for Mathematical_functions")
 
 data = [
-    (101, "Ayush", ["Python", "SQL", "Spark"]),
-    (102, "Riya", ["Excel", "SQL"]),
-    (103, "Karan", ["Python", "Java", "Scala"]),
-    (104, "Sneha", ["Python", "Spark"]),
-    (105, "Rahul", ["SQL", "Tableau"])
+    (101, "Ayush", "Python", "SQL", "Spark"),
+    (102, "Riya", "Excel", "SQL", None),
+    (103, "Karan", "Python", "Java", "Scala"),
+    (104, "Sneha", "Python", "Spark", None),
+    (105, "Rahul", "SQL", "Tableau", None)
 ]
 
-columns = ["emp_id", "name", "skills"]
+columns = ["emp_id", "name", "skill_set1", "skill_set2", "skill_set3"]
 
 df = spark.createDataFrame(data, columns)
-df.show(truncate=False)
+df.cache()
+df.select(array(col("skill_set1"),col("skill_set2")).alias("skills")).show()
