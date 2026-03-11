0615d66f61fb17c6bf0d5faf3c21ddbd92dfaa94
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-11 13:30:52 +0530

    value round down using floor()

--- src/mathematicalfunctions/math.py
+++ src/mathematicalfunctions/math.py
@@ -21,4 +21,5 @@ columns = ["id", "name", "value", "number"]
 df = spark.createDataFrame(data, columns)
 df.cache()
 #df.select("name", abs("value").alias("absolute_value")).show()
-df.select("name", ceil("value").alias("ceil_value")).show()
+#df.select("name", ceil("value").alias("ceil_value")).show()
+df.select("name", floor("value").alias("floor_value")).show()
