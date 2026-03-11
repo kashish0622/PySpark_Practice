ce1302f985de35ae0d89a1cff5959e50f46770a5
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-11 13:27:33 +0530

    ceil() added to round up the value

--- src/mathematicalfunctions/math.py
+++ src/mathematicalfunctions/math.py
@@ -20,5 +20,5 @@ columns = ["id", "name", "value", "number"]
 
 df = spark.createDataFrame(data, columns)
 df.cache()
-df.select("name", abs("value").alias("absolute_value")).show()
-
+#df.select("name", abs("value").alias("absolute_value")).show()
+df.select("name", ceil("value").alias("ceil_value")).show()
