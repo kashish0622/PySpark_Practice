775f951eafea3652c30b771e481eb472208f0036
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-22 02:03:51 +0530

    created a new spark session

--- src/spark_session/spark_session.py
+++ src/spark_session/spark_session.py
@@ -1,21 +1,10 @@
 from pyspark.sql import SparkSession
-
-spark = SparkSession.builder \
-    .appName("PySpark Practice") \
-    .master("local[*]") \
-    .config("spark.executor.memory", "2g") \
-    .config("spark.driver.memory", "1g") \
-    .config("spark.sql.shuffle.partitions", "200") \
+spark = SparkSession\
+    .builder\
+    .appName("Pyspark_Practice")\
+    .master("local[*]")\
+    .config("spark.driver.memory",'2g')\
+    .config("spark.executor.memory",'3g')\
+    .config("spark.executor.cores", 3)\
     .getOrCreate()
-
-print("Spark Session Created Successfully")
-
-from pyspark.sql import SparkSession
-
-# Creating SparkSession
-spark = SparkSession.builder \
-    .appName("PySpark Practice") \
-    .getOrCreate()
-
-# Display Spark version
-print("Spark Version:", spark.version)
+print("Spark session created successfully for pyspark practice")
